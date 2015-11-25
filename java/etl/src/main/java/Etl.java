e5028f836d57f8259c29c6c226e7b6ab8c62ac65
mattphillips <mattphillips1016@gmail.com>
2015-11-25 15:24:32 +0000

    Basic implementation

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,8 +1,20 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Etl {
-   public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-      return null;
-   }
+	public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+		
+		Map<String, Integer> newScoring = new HashMap<>();
+		
+		for (Integer key : old.keySet()) {
+
+			for (String s : old.get(key)) {
+
+				newScoring.put(s.toLowerCase(), key);
+			}
+		}
+
+		return newScoring;
+	}
 }
