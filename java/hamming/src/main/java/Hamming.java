d2fc3a5afce3d4e2f909f38140a8a1219bf28a55
mattphillips <mattphillips1016@gmail.com>
2015-12-01 19:28:22 +0000

    Implemented the hamming distance of two DNA strands

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,2 +1,18 @@
 public class Hamming {
+	
+	public static int compute(final String s, final String t) {
+		
+		if (s.length() != t.length())
+			throw new IllegalArgumentException();
+		
+		int distance = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != t.charAt(i)) {
+				distance++;
+			}
+		}
+		
+		return distance;
+	}
 }
