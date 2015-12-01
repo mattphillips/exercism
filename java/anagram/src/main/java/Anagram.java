6c2e159e5f5297e16e1224cc0965d6f84c86ff56
mattphillips <mattphillips1016@gmail.com>
2015-12-01 19:03:57 +0000

    Implemented anagram exercise

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Anagram {
+
+	private final String word;
+
+	public Anagram(final String word) {
+		this.word = word.toLowerCase();
+	}
+
+	public List<String> match(final List<String> possibleAnagrams) {
+		return possibleAnagrams.stream()
+				.filter(pa -> !(word.toLowerCase().equals(pa.toLowerCase()))) // check if the possible anagram is identical to the word
+				.filter(x -> sortString(word).equals(sortString(x))) // check the word and anagram are equal
+				.collect(Collectors.toList());
+	}
+
+	private String sortString(final String str) {
+		char[] sArr = str.toLowerCase().toCharArray();
+		Arrays.sort(sArr);
+		return new String(sArr);
+	}
 }
