4171f844bd53b906729addceacb3007e84945f49
mattphillips <mattphillips1016@gmail.com>
2015-12-14 15:16:13 +0000

    Refactored to make use of streams more and remove for loops

--- java/meetup/src/main/java/Meetup.java
+++ java/meetup/src/main/java/Meetup.java
@@ -1,8 +1,8 @@
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.joda.time.DateTime;
@@ -10,7 +10,6 @@ import org.joda.time.DateTime;
 public class Meetup {
 
 	private static final int FIRST_DAY_OF_MONTH = 1;
-	private static final int NUMBER_OF_DAYS_IN_WEEK = 7;
 	private static final int START_OF_WEEK = 6;
 	
 	private static final Integer[] FIRST_WEEK_DAYS = {1, 2, 3, 4, 5, 6, 7};
@@ -21,34 +20,32 @@ public class Meetup {
 	
 	private final int month;
 	private final int year;
+	private final int numberOfDaysInMonth;
+	private final Map<MeetupSchedule, Set<Integer>> monthSchedule;
 
 	public Meetup(final int month, final int year) {
 		this.month = month;
 		this.year = year;
+		numberOfDaysInMonth = new DateTime(year, month, 1, 0, 0).dayOfMonth().getMaximumValue();
+		monthSchedule = buildMonth(numberOfDaysInMonth);
 	}
 
 	public DateTime day(int day, MeetupSchedule schedule) {
-
-		int numOfDaysInMonth = new DateTime(year, month, 1, 0, 0).dayOfMonth().getMaximumValue();
-
-		Set<DateTime> possibleDates = new HashSet<>();
-		
-		for (int i = FIRST_DAY_OF_MONTH; i < numOfDaysInMonth; i += NUMBER_OF_DAYS_IN_WEEK) {
-
-			DateTime dateOfDayInWeek = new DateTime(year, month, i, 0, 0).withDayOfWeek(day);
-
-			if (dateOfDayInWeek.getMonthOfYear() == month)
-				possibleDates.add(dateOfDayInWeek);
-		}
-		
-		return possibleDates.stream()
-				.filter(d -> buildMonth(numOfDaysInMonth).get(schedule).contains(d.getDayOfMonth()))
+		return IntStream
+				.range(FIRST_DAY_OF_MONTH, numberOfDaysInMonth)
+				.mapToObj(i -> {
+					DateTime dateOfDayInWeek = new DateTime(year, month, i, 0, 0).withDayOfWeek(day);
+					if (dateOfDayInWeek.getMonthOfYear() == month)
+						return dateOfDayInWeek;
+					return null;
+				})
+				.filter(d -> d != null)
+				.filter(d -> monthSchedule.get(schedule).contains(d.getDayOfMonth()))
 				.findFirst()
 				.get();
 	}
 	
 	private Map<MeetupSchedule, Set<Integer>> buildMonth(int numberOfDays) {
-		
 		Map<MeetupSchedule, Set<Integer>> month = new HashMap<>();
 		month.put(MeetupSchedule.FIRST, buildWeek(FIRST_WEEK_DAYS));
 		month.put(MeetupSchedule.SECOND, buildWeek(SECOND_WEEK_DAYS));
@@ -64,10 +61,6 @@ public class Meetup {
 	}
 	
 	private Set<Integer> calculateLastWeekOfMonth(final int numberOfDays) {
-		Set<Integer> week = new HashSet<>();
-		for (int i = numberOfDays - START_OF_WEEK; i <= numberOfDays; i++) {
-			week.add(i);
-		}
-		return week;
+		return IntStream.range(numberOfDays - START_OF_WEEK, numberOfDays + 1).boxed().collect(Collectors.toSet());
 	}
 }
