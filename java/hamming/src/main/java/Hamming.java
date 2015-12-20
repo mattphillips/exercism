93c4420a15fe77dbb49dfdc74dd2c356ec8644b7
mattphillips <mattphillips1016@gmail.com>
2015-12-20 15:30:16 +0000

    Refactored to use a stream instead of for loop

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,17 +1,15 @@
+import java.util.stream.IntStream;
+
 public class Hamming {
 	
 	public static int compute(final String s, final String t) {
 		
 		if (s.length() != t.length())
 			throw new IllegalArgumentException();
-		
-		int distance = 0;
-		
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) != t.charAt(i))
-				distance++;
-		}
-		
-		return distance;
+
+		return IntStream
+				.range(0, s.length())
+				.map(i -> s.charAt(i) != t.charAt(i) ? 1 : 0)
+				.sum();
 	}
 }
