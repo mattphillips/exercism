81f57ebe362cda75dbfc7b56744204734d75e6e2
Matt Phillips <mattphillips1016@gmail.com>
2016-04-20 23:52:24 +0100

    changed to use rangeClosed which is inclusive of end

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -6,17 +6,16 @@ public class Pangrams {
 
     private static final int LOWERCASE_A = 'a';
     private static final int LOWERCASE_Z = 'z';
-    private static final int EXCLUSIVE_OFFSET = 1;
 
     public static boolean isPangram(String sentence) {
         Set<Character> characters = sentence.toLowerCase()
                 .chars()
-                .mapToObj(c -> (char)c)
+                .mapToObj(i -> (char)i)
                 .map(Character::new)
                 .collect(Collectors.toSet());
 
-        return IntStream.range(LOWERCASE_A, LOWERCASE_Z + EXCLUSIVE_OFFSET)
-                .mapToObj(c -> (char)c)
+        return IntStream.rangeClosed(LOWERCASE_A, LOWERCASE_Z)
+                .mapToObj(i -> (char)i)
                 .map(characters::contains)
                 .reduce((b1, b2) -> b1 && b2)
                 .orElse(false);
