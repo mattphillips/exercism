a0d540b4f31d5857a66e4d31ae51d209ce3a87d7
Matt Phillips <mattphillips1016@gmail.com>
2016-05-22 16:30:22 +0100

    completed bob exercise in scala

---
+++ scala/bob/src/test/scala/Bob.scala
@@ -0,0 +1,8 @@
+class Bob {
+  def hey(message: String) = message match {
+    case silence if message.replaceAll(" ", "").matches("") => "Fine. Be that way!"
+    case shouting if message.matches("[A-Z\\d\\W]+[^\\d\\?][\\?]?") =>  "Whoa, chill out!"
+    case question if message.endsWith("?") => "Sure."
+    case _ => "Whatever."
+  }
+}
