6abfa8de761a37f2e3e268af03b81c11cd8c90fe
Matt Phillips <mattphillips1016@gmail.com>
2016-05-22 16:37:25 +0100

    moved Bob.scala to correct package

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,6 +1,6 @@
 class Bob {
   def hey(message: String) = message match {
-    case silence if message.replaceAll(" ", "").matches("") => "Fine. Be that way!"
+    case silence if message.replaceAll(" ", "").isEmpty => "Fine. Be that way!"
     case shouting if message.matches("[A-Z\\d\\W]+[^\\d\\?][\\?]?") =>  "Whoa, chill out!"
     case question if message.endsWith("?") => "Sure."
     case _ => "Whatever."
