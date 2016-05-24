180bac09e0a6dd7881f6650c7d120706447304d7
Matt Phillips <mattphillips1016@gmail.com>
2016-05-24 16:51:30 +0100

    updated to use count instead of map -> sum

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,6 +1,6 @@
 object Hamming {
   def compute(s: String, t: String): Int = {
-    if (s.length == t.length) s zip t map { case (a: Char, b: Char) => if (a == b) 0 else 1 } sum
+    if (s.length == t.length) s zip t count { case (a, b) => a != b }
     else throw new IllegalArgumentException()
   }
 }
