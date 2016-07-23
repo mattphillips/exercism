3cd6dba36078058823a8e0a6700001fc3da1db86
Matt Phillips <mattphillips1016@gmail.com>
2016-07-23 11:18:45 +0200

    WIP pythargorean exercise

---
+++ ecmascript/pythagorean-triplet/pythagorean-triplet.js
@@ -0,0 +1,20 @@
+export default class Triplet {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  sum() {
+    return this.a + this.b + this.c;
+  }
+
+  product() {
+    // return this.a ** 2 + this.b ** 2 + this.c ** 2;
+    return this.a * this.b * this.c;
+  }
+
+  isPythagorean() {
+    return this.a ** 2 + this.b ** 2 === this.c ** 2;
+  }
+}
