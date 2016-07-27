0593c7c56bd338e83c355fb6e690e8b6e92078ac
Matt Phillips <mattphillips1016@gmail.com>
2016-07-27 18:52:32 +0100

    reduced functions to where clause

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -4,19 +4,9 @@ import Data.Char (toLower)
 import Data.List (sort)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor word candidates = filter (isNotIdenticalAndIsAnagram word) candidates
-
-isNotIdenticalAndIsAnagram :: String -> String -> Bool
-isNotIdenticalAndIsAnagram w c = not (isIdenticalWord w c) && isAnagram w c
-
-isIdenticalWord :: String -> String -> Bool
-isIdenticalWord w c = toLowerCase w == toLowerCase c
-
-isAnagram :: String -> String -> Bool
-isAnagram w c = sortString w == sortString c
-
-sortString :: String -> String
-sortString w = sort (toLowerCase w)
-
-toLowerCase :: String -> String
-toLowerCase s = map toLower s
+anagramsFor w = filter (\c -> isNotIdentical c && isAnagram c)
+  where
+    isNotIdentical c = toLowerCase w /= toLowerCase c
+    isAnagram c = sortString w == sortString c
+    sortString s = sort (toLowerCase s)
+    toLowerCase s = map toLower s
